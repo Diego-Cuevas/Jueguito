4a793f88f74f517dfb602f3f7a585f3e68b37018
DiegoEchauri <diegoechauri2@gmail.com>
2023-10-10 00:49:41 -0600

    Almacena UUID

--- Scrips/Interfaz.gd
+++ Scrips/Interfaz.gd
@@ -1,21 +1,36 @@
 extends CanvasLayer
 
-const ARCHIVO = "user://SAVEFILE.save"
+const archivo = "user://SAVEFILE.save"
 var url = 'http://cfetycoon-laravel.test/api/create_player'
 var user_name = ""
+var uuid = ""
 
 func _on_AddUser_request_completed(result, response_code, headers, body):
 	var response = body.get_string_from_utf8()
+	var uuid = response
+	var f = File.new()
+	f.open(archivo, File.WRITE)
+	f.store_var(uuid)
+	f.close()
+	
 	get_tree().change_scene("res://Tiles/contenedortilemap.tscn")
 	pass # Replace with function body.
 
 func _on_Iniciar_pressed():
-	user_name = $UserName.text
-	var data = {
-		"user_name": user_name
-	}
-	var envio = JSON.print(data)
-	$AddUser.request(url,["Content-Type: application/json"],true,HTTPClient.METHOD_POST, envio)
+	var f = File.new()
+	if(f.file_exists(archivo)):
+		f.open(archivo, File.READ)
+		uuid = f.get_var()
+		f.close()
+		print(uuid)
+	else:
+		print("No hay archivo")
+	#user_name = $UserName.text
+	#var data = {
+	#	"user_name": user_name
+	#}
+	#var envio = JSON.print(data)
+	#$AddUser.request(url,["Content-Type: application/json"],true,HTTPClient.METHOD_POST, envio)
 	#get_tree().change_scene("res://Tiles/contenedortilemap.tscn")
 	pass # Replace with function body.
 
