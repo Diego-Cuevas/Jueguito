baa74296ef0560c107af06abce3ddfe0a1a98241
Diego Echauri <diegoechauri2@gmail.com>
2023-11-13 18:27:17 -0600

    Borrando codigo basura

--- Scrips/global_var.gd
+++ Scrips/global_var.gd
@@ -18,38 +18,4 @@ var mouse = null
 var posible = false
 
 func _ready():
-	load_game()
-
-func save_game():
-	print("Guardando el archivo")
-	var save_file = File.new()
-	save_file.open(save_filename, File.WRITE)
-	var saved_nodes = get_tree().get_nodes_in_group("Saved")
-	for node in saved_nodes:
-		print(node.get_save_stats())
-		if node.filename.empty():
-			print("No hay nada que escribir")
-			break
-		var node_details = node.get_save_stats()
-		save_file.store_line(to_json(node_details))
-	save_file.close()
-
-func load_game():
-	print("Accediendo al archivo")
-	var save_file = File.new()
-	if not save_file.file_exists(save_filename):
-		return
-	var saved_nodes = get_tree().get_nodes_in_group("Saved")
-	for node in saved_nodes:
-		node.queue_free()
-	save_file.open(save_filename,File.READ)
-	while save_file.get_position() < save_file.get_len():
-		var node_data = parse_json(save_file.get_line())
-		print(node_data.filename)
-		var new_obj = load(node_data.filename).instance()
-		get_node(node_data.parent).call_deferred('add_child',new_obj)
-		new_obj.load_save_stats(node_data)
-		
-func _notification(what):
-	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
-		save_game()
+	pass
