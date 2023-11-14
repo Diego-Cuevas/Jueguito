a78c7f448f9075201bae8515e6699edf101b654e
Diego Echauri <diegoechauri2@gmail.com>
2023-11-13 18:29:34 -0600

    Borrando mas codigo

--- Tiles/Construcciones/Aerogenerador.gd
+++ Tiles/Construcciones/Aerogenerador.gd
@@ -15,14 +15,3 @@ func _process(_delta):
 	if $Tiempo_de_produccion.is_stopped():
 		$Tiempo_de_produccion.start()
 		GlobalVar.recursos["Energía"] += Produccion
-		
-func get_save_stats():
-	return{
-		'file_name' : get_filename(),
-		'parent' : get_parent().get_path(),
-		'x_pos' : position.x,
-		'y_pos' : position.y
-	}
-	
-func load_save_stats(stats):
-	position = Vector2(stats.x_pos,stats.y_pos)
